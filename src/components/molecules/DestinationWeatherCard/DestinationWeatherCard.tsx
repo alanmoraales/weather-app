c4212a44d58a521143b30843769c72dcda237bb6
alanmoraales <alanmoraales845@gmail.com>
2022-03-13 01:37:42 -0600

    add skeleton in DestinationWeatherCard component

--- src/components/molecules/DestinationWeatherCard/DestinationWeatherCard.tsx
+++ src/components/molecules/DestinationWeatherCard/DestinationWeatherCard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Flex, Grid } from "@chakra-ui/react";
+import { Flex, Grid, Skeleton } from "@chakra-ui/react";
 import Heading from "@atoms/Heading";
 import Body from "@atoms/Body";
 
@@ -30,16 +30,28 @@ const DestinationWeatherCard = ({
       <Flex gap={4} alignItems="center">
         <Grid gap={1} placeItems="center">
           <Body variant="label">Min</Body>
-          <Heading variant="h6" color="primary">
-            {minTemperature}
-          </Heading>
+          <Skeleton
+            isLoaded={Boolean(minTemperature)}
+            height="19.19px"
+            width="100%"
+          >
+            <Heading variant="h6" color="primary">
+              {minTemperature}
+            </Heading>
+          </Skeleton>
         </Grid>
         <Body>-</Body>
         <Grid gap={1} placeItems="center">
           <Body variant="label">Max</Body>
-          <Heading variant="h6" color="primary">
-            {maxTemperature}
-          </Heading>
+          <Skeleton
+            isLoaded={Boolean(maxTemperature)}
+            height="19.19px"
+            width="100%"
+          >
+            <Heading variant="h6" color="primary">
+              {maxTemperature}
+            </Heading>
+          </Skeleton>
         </Grid>
       </Flex>
     </Flex>
